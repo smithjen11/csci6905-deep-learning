3dc806ecec954078f246930418c5c8b01e5e5d29
Jennifer Smith <smithjen11@students.ecu.edu>
2018-06-15 11:29:30 -0400

    Fix errors in model; model accuracy is very bad

--- cnn/cnn_mnist.py
+++ cnn/cnn_mnist.py
@@ -34,7 +34,6 @@ ROOT_PATH = "/vagrant"
 
 
 def cnn_model_fn(features, labels, mode):
-  print('model 1')
   """Model function for CNN."""
   # Input Layer
   # Reshape X to 4-D tensor: [batch_size, width, height, channels]
@@ -54,15 +53,11 @@ def cnn_model_fn(features, labels, mode):
       data_format="channels_last",
       activation=tf.nn.relu)
 
-  print('model 2')
-
   # Pooling Layer #1
   # First max pooling layer with a 2x2 filter and stride of 2
   # Input Tensor Shape: [batch_size, 28, 28, 32]
   # Output Tensor Shape: [batch_size, 14, 14, 32]
   pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
-
-  print('model 3')
 
   # Convolutional Layer #2
   # Computes 64 features using a 5x5 filter.
@@ -77,22 +72,16 @@ def cnn_model_fn(features, labels, mode):
       data_format="channels_last",
       activation=tf.nn.relu)
 
-  print('model 4')
-
   # Pooling Layer #2
   # Second max pooling layer with a 2x2 filter and stride of 2
   # Input Tensor Shape: [batch_size, 14, 14, 64]
   # Output Tensor Shape: [batch_size, 7, 7, 64]
   pool2 = tf.layers.max_pooling2d(inputs=conv2, pool_size=[2, 2], strides=2)
 
-  print('model 5')
-
   # Flatten tensor into a batch of vectors
   # Input Tensor Shape: [batch_size, 7, 7, 64]
   # Output Tensor Shape: [batch_size, 7 * 7 * 64]
   pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
-
-  print('model 6')
 
   # Dense Layer
   # Densely connected layer with 1024 neurons
@@ -100,20 +89,14 @@ def cnn_model_fn(features, labels, mode):
   # Output Tensor Shape: [batch_size, 1024]
   dense = tf.layers.dense(inputs=pool2_flat, units=1024, activation=tf.nn.relu)
 
-  print('model 7')
-
   # Add dropout operation; 0.6 probability that element will be kept
   dropout = tf.layers.dropout(
       inputs=dense, rate=0.4, training=mode == tf.estimator.ModeKeys.TRAIN)
 
-  print('model 8')
-
   # Logits layer
   # Input Tensor Shape: [batch_size, 1024]
   # Output Tensor Shape: [batch_size, 10]
-  logits = tf.layers.dense(inputs=dropout, units=10)
-
-  print('model 9')
+  logits = tf.layers.dense(inputs=dropout, units=101)
 
   predictions = {
       # Generate predictions (for PREDICT and EVAL mode)
@@ -123,17 +106,11 @@ def cnn_model_fn(features, labels, mode):
       "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
   }
 
-  print('model 10')
-
   if mode == tf.estimator.ModeKeys.PREDICT:
     return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
 
-  print('model 11')
-
   # Calculate Loss (for both TRAIN and EVAL modes)
   loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
-
-  print('model 12')
 
   # Configure the Training Op (for TRAIN mode)
   if mode == tf.estimator.ModeKeys.TRAIN:
@@ -143,8 +120,6 @@ def cnn_model_fn(features, labels, mode):
         global_step=tf.train.get_global_step())
     return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
 
-  print('model 13')
-
   # Add evaluation metrics (for EVAL mode)
   eval_metric_ops = {
       "accuracy": tf.metrics.accuracy(
@@ -153,41 +128,75 @@ def cnn_model_fn(features, labels, mode):
       mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
 
 
-def load_data(data_directory):
+def load_training_data(data_directory):
   directories = [d for d in os.listdir(data_directory) 
                  if os.path.isdir(os.path.join(data_directory, d))]
   labels = []
   images = []
+  label_names = []
+  count = 0
   for d in directories:
+    label_names.append(d)
     label_directory = os.path.join(data_directory, d)
     file_names = [os.path.join(label_directory, f) 
                   for f in os.listdir(label_directory) 
                   if f.endswith(".jpg")]
+
+    for f in file_names[:10]:
+      images.append(skimage.data.imread(f))
+      labels.append(count)
+
+    count = count + 1
+  return images, labels
+
+
+def load_eval_data(data_directory):
+  directories = [d for d in os.listdir(data_directory) 
+                 if os.path.isdir(os.path.join(data_directory, d))]
+  labels = []
+  images = []
+  label_names = []
+  count = 0
+  for d in directories:
+    label_names.append(d)
+    label_directory = os.path.join(data_directory, d)
+    file_names = [os.path.join(label_directory, f) 
+                  for f in os.listdir(label_directory) 
+                  if f.endswith(".jpg")]
+
     for f in file_names:
       images.append(skimage.data.imread(f))
-      labels.append(d)
+      labels.append(count)
+
+    count = count + 1
   return images, labels
 
 
 def main(unused_argv):
   # Load training and eval data
   # mnist = tf.contrib.learn.datasets.load_dataset("mnist")
-  train_data_directory = os.path.join(ROOT_PATH, "101_ObjectCategories")
-  test_data_directory = os.path.join(ROOT_PATH, "101_ObjectCategories")
-  images, labels = load_data(train_data_directory)
+  data_directory = os.path.join(ROOT_PATH, "101_ObjectCategories")
+  
+  ## Training data
+  images, labels = load_training_data(data_directory)
 
   # Resize images
   images28 = [transform.resize(image, (28, 28, 3)) for image in images]
   images28 = np.array(images28, dtype=np.float32)
 
   train_data = images28  # Returns np.array
-  # train_labels = np.asarray(labels, dtype=np.int32)
-  train_labels = np.arange(start=0, stop=len(set(labels)), dtype=np.int32)
-  eval_data = images28  # Returns np.array
-  # eval_labels = np.asarray(labels, dtype=np.int32)
-  eval_labels = np.arange(start=0, stop=len(set(labels)), dtype=np.int32)
+  train_labels = np.asarray(labels, dtype=np.int32)
 
-  print('hi')
+  ## Eval data
+  eval_images, eval_labels = load_eval_data(data_directory)
+
+  # Resize images
+  eval_images28 = [transform.resize(image, (28, 28, 3)) for image in eval_images]
+  eval_images28 = np.array(eval_images28, dtype=np.float32)
+  # train_labels = np.arange(start=0, stop=len(set(labels)), dtype=np.int32)
+  eval_data = eval_images28  # Returns np.array
+  eval_labels = np.asarray(eval_labels, dtype=np.int32)
+  # eval_labels = np.arange(start=0, stop=len(set(labels)), dtype=np.int32)
 
   # Create the Estimator
   classifier = tf.estimator.Estimator(
@@ -199,8 +208,6 @@ def main(unused_argv):
   logging_hook = tf.train.LoggingTensorHook(
       tensors=tensors_to_log, every_n_iter=50)
 
-  print('hi 2')
-
   # Train the model
   train_input_fn = tf.estimator.inputs.numpy_input_fn(
       x={"x": train_data},
@@ -208,13 +215,10 @@ def main(unused_argv):
       batch_size=100,
       num_epochs=None,
       shuffle=True)
-  print('hi 3')
   classifier.train(
       input_fn=train_input_fn,
       steps=200,
       hooks=[logging_hook])
-
-  print('hi 4')
 
   # Evaluate the model and print results
   eval_input_fn = tf.estimator.inputs.numpy_input_fn(
@@ -222,7 +226,7 @@ def main(unused_argv):
       y=eval_labels,
       num_epochs=1,
       shuffle=False)
-  eval_results = mnist_classifier.evaluate(input_fn=eval_input_fn)
+  eval_results = classifier.evaluate(input_fn=eval_input_fn)
   print(eval_results)
 
 
